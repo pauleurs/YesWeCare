098b0a56f004f1ccccdc9deca9d9d79d773856c1
Paul-Emile Leurs <paul-emile.leurs@hotmail.fr>
2022-06-02 10:43:20 +0200

    add onbording scrool view

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutterfire_ui/auth.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
@@ -24,14 +23,14 @@ class _OnboardingState extends State<Onboarding> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (_formKey.currentState!.validate()) {
-            String newName = '${firstName!} ${lastName!}';
+            String newName = '${firstName!.trim()} ${lastName!.trim()}';
             user!.updateDisplayName(newName);
 
             Navigator.pushNamed(context, '/home');
           }
         },
         backgroundColor: Colors.black,
-        child: const Icon(Icons.skip_next),
+        child: const Icon(Icons.arrow_right_alt),
       ),
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -56,68 +55,70 @@ class _OnboardingState extends State<Onboarding> {
       minimum: const EdgeInsets.all(15),
       child: Form(
         key: _formKey,
-        child: Column(
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 3,
-              child: Image.network(
-                  'https://cdn.voelkl.com/storage/user_upload/voelkl/Sustainability/Yes-we-care-Voelkl.png'),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              initialValue: user?.email,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) => lastName = value,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email *',
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 3,
+                child: Image.network(
+                    'https://cdn.voelkl.com/storage/user_upload/voelkl/Sustainability/Yes-we-care-Voelkl.png'),
               ),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your first name';
-                }
-                return null;
-              },
-              keyboardType: TextInputType.number,
-              onChanged: (value) => phone = value,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Phone *',
+              const SizedBox(height: 15),
+              TextFormField(
+                initialValue: user?.email,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) => lastName = value,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email *',
+                ),
               ),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your last name';
-                }
-                return null;
-              },
-              onChanged: (value) => firstName = value,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'First name *',
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                onChanged: (value) => phone = value,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Phone *',
+                ),
               ),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your first name';
-                }
-                return null;
-              },
-              onChanged: (value) => lastName = value,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Last name *',
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
+                onChanged: (value) => firstName = value,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'First name *',
+                ),
               ),
-            ),
-            const SizedBox(height: 15),
-          ],
+              const SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+                onChanged: (value) => lastName = value,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Last name *',
+                ),
+              ),
+              const SizedBox(height: 15),
+            ],
+          ),
         ),
       ),
     );
